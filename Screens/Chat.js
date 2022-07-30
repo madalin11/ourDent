ae5cf2c9692d0d93af86d4d310e4160b3b63a481
madalin11 <madalin.c.puiu@gmail.com>
2022-07-30 17:05:53 +0300

    update chat

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -19,20 +19,31 @@ const Chat = ({ navigation }) => {
       friendPhoto: friendPhoto
     });
   }
+  const handleSignOut = () => {
+    auth
+        .signOut()
+        .then(() => {
+            navigation.reset({
+                index: 0,
+                routes: [{ name: 'Login screen' }],
+            });
+        })
+        .catch(error => alert(error.message))
+}
   useEffect(() => {
     const unsubscribe = db
       .collection("peoples")
       .doc(temp)
-      .collection("doctors").onSnapshot(snapshot => 
+      .collection("doctors").onSnapshot(snapshot =>
         setFriends(
           snapshot.docs.map((doc) => ({
             id: doc.id,
             data: doc.data()
           }))
         )
-        
+
       )
-      console.log(friends)
+    console.log(friends)
     return unsubscribe;
   }, [navigation])
 
@@ -63,23 +74,23 @@ const Chat = ({ navigation }) => {
 
   function filterZZZ(friend) {
     try {
-        if (friend.data.name == '') {
-            return true;
-        }
-        try {
+      if (friend.data.name == '') {
+        return true;
+      }
+      try {
 
-            if (friend.data.name.toLowerCase().includes(textSearch.toLowerCase()))
-                return true;
+        if (friend.data.name.toLowerCase().includes(textSearch.toLowerCase()))
+          return true;
 
-        } catch (err) {
+      } catch (err) {
 
-        }
-        return false
+      }
+      return false
     } catch (err) {
 
     }
     return true
-}
+  }
   return (
     <View style={styles.container}>
       <LinearGradient
@@ -88,11 +99,14 @@ const Chat = ({ navigation }) => {
         style={styles.background}
       />
 
-      <View style={{ flexDirection: 'column', marginTop: 85 }}>
-        <Text style={{ fontSize: 40, textAlign: 'center', color: 'white', fontWeight: '500', fontFamily: 'Times New Roman', shadowColor: '#202020', shadowOpacity: 1, shadowOffset: { height: 2 } }}>
+      <View style={{ flexDirection: 'row', marginTop: 85 }}>
+      <TouchableOpacity onPress={handleSignOut} style={{ alignSelf: 'center', marginLeft:20}}>
+          <Image style={{ width: 20, height: 20,tintColor:'black' }} source={require('../Icons/leftarrow.png')}></Image>
+        </TouchableOpacity>
+        <Text style={{ fontSize: 40,flex:1, textAlign: 'center', color: 'white', fontWeight: '500', fontFamily: 'Times New Roman', shadowColor: '#202020', shadowOpacity: 1, shadowOffset: { height: 2 } }}>
           Chat
         </Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Add chat")} style={{ alignSelf: 'flex-end', marginRight: 20, top: -23 }}>
+        <TouchableOpacity onPress={() => navigation.navigate("Add chat")} style={{ alignSelf: 'center', marginRight: 20}}>
           <Image style={{ width: 20, height: 20 }} source={require('../Icons/newchat.png')}></Image>
         </TouchableOpacity>
       </View>
@@ -117,11 +131,11 @@ const Chat = ({ navigation }) => {
 
       <ScrollView style={{ height: '100%' }}>
 
-      {
-                    friendsAdd.filter(filterZZZ).map(({ id, data: { name, profilePhoto } }) => (
-                        <ChatListItem key={id} enterChat={enterChat} friendName={name} id={id} friendPhoto={profilePhoto} />
-                    ))
-                }
+        {
+          friendsAdd.filter(filterZZZ).map(({ id, data: { name, profilePhoto } }) => (
+            <ChatListItem key={id} enterChat={enterChat} friendName={name} id={id} friendPhoto={profilePhoto} />
+          ))
+        }
 
 
       </ScrollView>
